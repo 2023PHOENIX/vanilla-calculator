cb40b0b9730060922dbc432d289d571175e32761
abhishek <abhishekshrivastav1920@gmail.com>
2023-09-07 10:54:38 +0530

    designing

--- main.js
+++ main.js
@@ -1,41 +1,37 @@
-const buttons = document.querySelectorAll('.button-item');
-
-
-
+const buttons = document.querySelectorAll(".button-item");
 
 for (let button of buttons) {
+  button.addEventListener("click", (e) => {
+    const instruction = e.target.innerText;
 
-    button.addEventListener('click', (e) => {
-            const instruction = e.target.innerText;
+    if (instruction === "DEL") {
+      updateResult(instruction);
+    } else if (instruction === "RESET") {
+      updateResult(instruction);
+    } else if (instruction === "=") {
+       const str = document.querySelector(".result").innerText;
+       console.log(str);
+      let x = eval(str);
+
+      console.log(x);
 
 
-            if (instruction === 'DEL') {
-                updateResult(instruction);
-            } else if (instruction === 'RESET') {
-                updateResult(instruction)
-            } else if (instruction === '=') {
-                let x = eval(document.querySelector('.result').innerHTML);
-
-                document.querySelector('.result').innerHTML = x;
-            } else {
-                updateResult(instruction, e.target.innerText);
-            }
-        }
-    )
+      document.querySelector(".result").innerHTML = x;
+    } else {
+      updateResult(instruction, e.target.innerText);
+    }
+  });
 }
-
 
 function updateResult(instruction, val) {
-    const result = document.querySelector('.result');
-    if (instruction === 'RESET') {
-        document.querySelector('.result').innerHTML = "";
-    } else if (instruction === 'DEL') {
-
-        document.querySelector('.result').innerHTML = document.querySelector('.result').innerHTML.slice(0, -1);
-    } else {
-        result.innerHTML = result.innerHTML + val;
-    }
-
+  const result = document.querySelector(".result");
+  if (instruction === "RESET") {
+    document.querySelector(".result").innerHTML = "";
+  } else if (instruction === "DEL") {
+    document.querySelector(".result").innerHTML = document.querySelector(
+      ".result",
+    ).innerHTML.slice(0, -1);
+  } else {
+    result.innerHTML = result.innerHTML + val;
+  }
 }
-
-
